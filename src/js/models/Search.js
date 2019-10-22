409c4255f4445f86cbaa3c9ac8a18ff6ff48a912
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-10-23 02:07:01 +0530

    Add error handling logic

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,23 +1,17 @@
 import { KEY, BASE_URL } from '../config';
 
-// export default class Search {
-//   constructor(query) {
-//     this.query = query;
-//   }
-
-//   getResults = () => {
-//     console.log('ahha');
-//   };
-// }
-
 export default class Search {
   constructor(query) {
     this.query = query;
   }
 
   async getResults() {
-    const res = await fetch(`${BASE_URL}q=${this.query}&appid=${KEY}`);
-    const data = await res.json();
-    return data;
+    try {
+      const res = await fetch(`${BASE_URL}q=${this.query}&appid=${KEY}`);
+      const data = await res.json();
+      this.result = data;
+    } catch (error) {
+      alert(error);
+    }
   }
 }
