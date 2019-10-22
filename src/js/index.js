3148876f5b9153ea40cc144d1b960c0794936672
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-10-23 03:54:50 +0530

    Add basic markup

--- src/js/index.js
+++ src/js/index.js
@@ -10,16 +10,16 @@ const state = {};
  * SEARCH CONTROLLER
  */
 
-state.search = new Search('New Delhi');
+// state.search = new Search('New Delhi');
 
-document.querySelector('.search__btn').addEventListener('click', async e => {
-  e.preventDefault();
-  const query = document.querySelector('#search__input').value;
-  try {
-    const result = await state.search.getResults(query);
-    console.log(result);
-  } catch (error) {
-    // eslint-disable-next-line no-alert
-    alert(error);
-  }
-});
+// document.querySelector('.search__btn').addEventListener('click', async e => {
+//   e.preventDefault();
+//   const query = document.querySelector('#search__input').value;
+//   try {
+//     const result = await state.search.getResults(query);
+//     console.log(result);
+//   } catch (error) {
+//     // eslint-disable-next-line no-alert
+//     alert(error);
+//   }
+// });
