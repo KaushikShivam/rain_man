9556a64493e6343d9b0dbccb25b2de1129b23fcc
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-11 17:44:29 +0530

    Add https

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,2 @@
 export const KEY = '98acf4edff3eebfa3ce173e73fc696cb';
-export const BASE_URL = 'http://api.openweathermap.org/data/2.5/forecast?';
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/forecast?';
